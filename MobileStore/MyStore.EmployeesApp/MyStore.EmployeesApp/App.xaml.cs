e10ef9bdc18e6b258ba6aae1501b7e237f936f38
samleo88 <59632639+samleo88@users.noreply.github.com>
2022-10-13 15:54:10 -0500

    init

---
+++ MobileStore/MyStore.EmployeesApp/MyStore.EmployeesApp/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+using MyStore.Models;
+using MyStore.EmployeesApp.Services;
+
+namespace MyStore.EmployeesApp
+{
+    public partial class App : Application
+    {
+        public static EmployeeDTO CurrentEmployee;
+        public static SignalRService SignalRService;
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Pages.LoginPage());
+        }
+    }
+}
